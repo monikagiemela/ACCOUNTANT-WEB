6a8e6767d050ddc2f7ca4567caf24b9123fe88a9
Monika Giemela <monikagiemela@gmail.com>
2022-06-16 10:25:00 +0200

    Bug fixes in models Storage

--- app/models.py
+++ app/models.py
@@ -3,19 +3,19 @@ from app import db
 
 class Accountants(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
-    hash = db.Column(db.Text, unique=True, nullable=False)
+    hash = db.Column(db.String, unique=True, nullable=False)
     current_balance = db.Column(db.Float, nullable=False, default=0)
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False,autoincrement=True)
-    email_address = db.Column(db.Text, unique=True, nullable=False)
-    hash = db.Column(db.Text, unique=True, nullable=False)
+    email_address = db.Column(db.String(80), unique=True, nullable=False)
+    hash = db.Column(db.String, unique=True, nullable=False)
     accountant_id = db.Column(db.Integer, db.ForeignKey('accountants.id'), nullable=False)
 
 class History(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False,autoincrement=True)
-    transaction = db.Column(db.Text, nullable=False)
-    product_name = db.Column(db.Text, nullable=True)
+    transaction = db.Column(db.String(8), nullable=False)
+    product_name = db.Column(db.String(80), nullable=True)
     quantity = db.Column(db.Float, nullable=True)
     price = db.Column(db.Float, nullable=True)
     sale_price = db.Column(db.Float, nullable=True)
@@ -26,7 +26,7 @@ class History(db.Model):
      
 class Storage(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False,autoincrement=True)
-    product_name= db.Column(db.Text, db.ForeignKey('users.id'), nullable=False)
+    product_name= db.Column(db.String(80), nullable=False)
     quantity = db.Column(db.Float, nullable=True)
     price = db.Column(db.Float, nullable=False)
     sale_price = db.Column(db.Float, nullable=False)
